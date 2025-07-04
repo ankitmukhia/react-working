0165a892534241e9fe0f549aac29cea6f7e21ef8
ankitmukhia <ankitmukhia157@gmail.com>
2025-07-04 17:39:51 +0530

    :::::

--- render-function/index.js
+++ render-function/index.js
@@ -1,53 +1,74 @@
 // Helps use adding stuff to the DOM
 
 function createElement(type, props, ...children) {
-  return {
-    type,
-    props: {
-      ...props,
-      children: children.map(child =>
-        typeof child === "object" ? child : createTextElement(child)
-      )
-    }
-  };
+	return {
+		type,
+		props: {
+			...props,
+			children: children.map(child =>
+				typeof child === "object" ? child : createTextElement(child)
+			)
+		}
+	};
 }
 
 function createTextElement(text) {
-  return {
-    type: "TEXT_ELEMENT",
-    props: {
-      nodeValue: text,
-      children: []
-    }
-  };
+	return {
+		type: "TEXT_ELEMENT",
+		props: {
+			nodeValue: text,
+			children: []
+		}
+	};
 }
 
 function render(element, container) {
-  const dom =
-    element.type == "TEXT_ELEMENT"
-      ? document.createTextNode("")
-      : document.createElement(element.type);
-  const isProperty = key => key !== "children";
-  Object.keys(element.props)
-    .filter(isProperty)
-    .forEach(name => {
-      dom[name] = element.props[name];
-    });
-  element.props.children.forEach(child => render(child, dom));
-  container.appendChild(dom);
+	const dom =
+		element.type == "TEXT_ELEMENT"
+			? document.createTextNode("")
+			: document.createElement(element.type);
+	const isProperty = key => key !== "children";
+	Object.keys(element.props)
+		.filter(isProperty)
+		.forEach(name => {
+			dom[name] = element.props[name];
+		});
+	/**
+	 * @example
+	 * const element = {
+	 *   type: "div",
+	 *   props: {
+	 *		 children: [
+	 *				{
+	 *					type: "h1",
+	 *					props: {
+	 *						children: [
+	 *							{
+	 *								type: "TEXT_ELEMENT",
+	 *								props: {}
+	 *							}
+	 *						]
+	 *					}
+	 *				}
+	 *		 ]
+	 *   }
+	 * }
+	 **/
+	element.props.children.forEach(child => render(child, dom));
+	container.appendChild(dom);
 }
 
 const Didact = {
-  createElement,
-  render
+	createElement,
+	render
 };
 
 /** @jsx Didact.createElement */
 const element = (
-  <div style="background: salmon">
-    <h1>Hello World</h1>
-    <h2 style="text-align:right">from Didact</h2>
-  </div>
+	<div style="background: salmon">
+		<h1>Hello World</h1>
+		<h2 style="text-align:right">from Didact</h2>
+	</div>
 );
 const container = document.getElementById("root");
 Didact.render(element, container);
